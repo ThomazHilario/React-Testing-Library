e5fce83435b43faa1fbad504030bf72d9f041d3c
Thomaz Alves <thomazhilario5@gmail.com>
2024-05-24 10:55:37 -0300

    perf: newFiles
    - Refatorando o codigo.

--- src/components/newFile/index.tsx
+++ src/components/newFile/index.tsx
@@ -1,16 +1,54 @@
-// import css
-import { useState } from 'react'
+// import react
+import { FormEvent, useState } from 'react'
 
+// import css
 import './newfiles.css'
 
 export const NewFile = () => {
 
-    const [value, setValue] = useState<string>()
+    const [value, setValue] = useState<string>('')
 
-    function sendFileInLocalStorage(e){
+    // Enviando arquivo para localStorage
+    function sendFileInLocalStorage(e:FormEvent){
+        // Evitando a pagina atualizar
         e.preventDefault()
+        
+        // Evitando a pagina atualizar
+        e.preventDefault()
+        
+        // Logica da localStorage
+        const myArr = localStorage.getItem('files') !== null ? JSON.parse(localStorage.getItem('files') as string) : []
+        
+        // Jogando o novo file para o array de files
+        myArr.push({
+            name:value,
+            date: Date.now()
+        })
 
-        if(localStorage.getItem('files') !== null){
+        // Salvar na localStorage
+        localStorage.setItem('files', JSON.stringify(myArr))
+        
+        // Resetando a state value ao valor inicial
+        setValue('')
+    }
+
+    return(
+        <form onSubmit={sendFileInLocalStorage}>
+            <div>
+                <label>Name File:</label>
+                <textarea value={value} onChange={(e) => setValue(e.target.value)} required/>
+            </div>
+
+            <div>
+                <button type="submit" id='send__File__Btn'>Create New File</button>
+            </div>
+        </form>
+    )
+}
+
+/*
+
+    if(localStorage.getItem('files') !== null){
             // Buscando o que tem na localStorage
             const filesArr = JSON.parse(localStorage.getItem('files') as string)
 
@@ -41,18 +79,5 @@ export const NewFile = () => {
             // SAlvando na localStorage
             localStorage.setItem('files', JSON.stringify(filesArr))
         }
-    }
 
-    return(
-        <form onSubmit={sendFileInLocalStorage}>
-            <div>
-                <label>Name File:</label>
-                <input type="text" value={value} onChange={(e) => setValue(e.target.value)} required/>
-            </div>
-
-            <div>
-                <button type="submit" id='send__File__Btn'>Create New File</button>
-            </div>
-        </form>
-    )
-}
+*/
